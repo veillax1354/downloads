9358a1175f94c919276c6f810340c8926db9ab6f
Veillax <arp.putland@gmail.com>
2023-09-07 12:34:53 -0600

    Update index.py

--- files/index.py
+++ files/index.py
@@ -6,8 +6,9 @@ index = []
 
 for filename in os.listdir(repo_dir):
     if os.path.isfile(os.path.join(repo_dir, filename)):
-        description = input(f"Enter description for {filename}: ")
-        index.append({"filename": filename, "description": description})
+        # Split the filename into name and extension
+        name, extension = os.path.splitext(filename)
+        index.append({"name": name, "extension": extension})
 
 with open("__index__.json", "w") as f:
     json.dump(index, f, indent=4)
